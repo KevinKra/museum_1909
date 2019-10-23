44f85a2d29f76c26be548483d669e14072da2cff
Kevin Krato <kratok08@gmail.com>
2019-10-23 09:16:20 -0600

    Complete all steps for iteration 1

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -5,11 +5,16 @@ require_relative "../lib/exhibit.rb"
 class ExhibitTest < Minitest::Test
 
   def setup
-    @exhibit = Exhibit.new
+    @exhibit = Exhibit.new("Gems and Minerals", 0)
   end
 
   def test_it_exists
     assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_can_read_attributes
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
   end
 
 end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -5,11 +5,24 @@ require_relative "../lib/patron.rb"
 class PatronTest < Minitest::Test
 
   def setup
-    @patron = Patron.new
+    @patron = Patron.new("Bob", 20)
   end
 
   def test_it_exists
     assert_instance_of Patron, @patron
+  end
+
+  def test_it_can_read_attributes
+    assert_equal "Bob", @patron.name
+    assert_equal 20, @patron.spending_money
+    assert_equal [], @patron.interests
+  end
+
+  def test_it_can_add_interests
+    assert_equal [], @patron.interests
+    @patron.add_interest("Dead Sea Scrolls")
+    @patron.add_interest("Gems and Minerals")
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron.interests
   end
 
 end
