55d29af1de6175bdff1abae1a2617b1dc89aaf93
Kevin Krato <kratok08@gmail.com>
2019-10-23 09:34:21 -0600

    Write test and method for exhibit recommendations

--- lib/musuem.rb
+++ lib/musuem.rb
@@ -8,4 +8,10 @@ class Musuem
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    patron.interests.map do |interest|
+      @exhibits.find {|exhibit| exhibit.name == interest}
+    end
+  end
 end
--- test/musuem_test.rb
+++ test/musuem_test.rb
@@ -2,11 +2,15 @@ require "minitest/autorun"
 require "minitest/pride"
 require_relative "../lib/musuem.rb"
 require_relative "../lib/exhibit.rb"
+require_relative "../lib/patron.rb"
 
 class MusuemTest < Minitest::Test
 
   def setup
     @musuem = Musuem.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15) 
   end
 
   def test_it_exists
@@ -19,14 +23,24 @@ class MusuemTest < Minitest::Test
   end
 
   def test_it_can_add_exhibits
-    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
-    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
-    imax = Exhibit.new("IMAX", 15) 
     assert_equal [], @musuem.exhibits
-    @musuem.add_exhibit(gems_and_minerals) 
-    @musuem.add_exhibit(dead_sea_scrolls) 
-    @musuem.add_exhibit(imax) 
-    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], @musuem.exhibits
+    @musuem.add_exhibit(@gems_and_minerals) 
+    @musuem.add_exhibit(@dead_sea_scrolls) 
+    @musuem.add_exhibit(@imax) 
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @musuem.exhibits
+  end
+
+  def test_it_can_recommend_exhibits
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls") 
+    bob.add_interest("Gems and Minerals")
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")  
+    @musuem.add_exhibit(@gems_and_minerals) 
+    @musuem.add_exhibit(@dead_sea_scrolls) 
+    @musuem.add_exhibit(@imax) 
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @musuem.recommend_exhibits(bob)
+    assert_equal [@imax], @musuem.recommend_exhibits(sally)
   end
 
 end
